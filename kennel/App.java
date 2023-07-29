0a97df8e0e9f8cd3abc227e829207f3b51585a17
Oleg-GH <fan2004g@bk.ru>
2023-07-29 14:39:44 +0500

    App.java and Main.java were added

---
+++ kennel/App.java
@@ -0,0 +1,40 @@
+package kennel;
+
+import kennel.controller.KennelAccounting;
+import kennel.storage.KennelStorage;
+import kennel.view.ConsoleView;
+import kennel.view.View;
+
+public class App {
+    /**
+     * Основной цикл приложения, где происходит обработка основных команд
+     * (добавть животное, показать команды животного, выписать из питомника)
+     */
+    public static void run() {
+        KennelAccounting kennelAccounting = new KennelAccounting(new KennelStorage());
+        View view = new ConsoleView(kennelAccounting);
+
+        while (true) {
+            view.showKennelRegistry();
+            View.MainMenuCommand code = view.showMainMenuWithResult();
+            switch (code) {
+                case ADD_ANIMAL: 
+                    boolean result = view.showAddAnimalDialog();
+                    String resMessage = result ? "Животное добавлено" : "Не удалось добавить животное";
+                    System.out.println(resMessage);
+                
+                case SHOW_SKILLS:
+                    view.showDetailInfoAnimalDialog();
+                
+                case REMOVE_ANIMAL:
+                    int removeId = view.showRemoveAnimalDialog();
+                    String resMessage1 = removeId > -1  ? "Выписано животное" + removeId : "Не удалось  выписать животное";
+                    System.out.println(resMessage1);
+                
+                case EXIT:
+                    System.out.println("See you... )");
+                    return;                
+            }
+        }
+    }
+}
---
+++ kennel/Main.java
@@ -0,0 +1,9 @@
+package kennel;
+
+import kennel.util.Counter;
+
+public class Main {
+    public static void main(String[] args) {
+        App.run();
+    }
+}
